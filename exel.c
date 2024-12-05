3ab31cd453377e3e2916451b68633d1be600183f
stanislaw_janocha <janochastanislaw2004@gmail.com>
2024-12-05 09:37:10 +0100

    nie działa otwieranie pliku

--- exel.c
+++ exel.c
@@ -4,13 +4,14 @@
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>  
+#include <stdlib.h>
  
  //deklaracja enum
 enum{KONSOLA=1, PLIK=2, WYJSCIE =3 } stan_pracy;
 
 
 int main(void){
-
+    printf("\n=================================\n");
     //definicja zmiennych int
         int czas_dyskretny;
 
@@ -25,20 +26,41 @@ int main(void){
             printf("3. Wyjscie \n");
             scanf("%d", (int* )&stan_pracy);
 
+    printf("\n=================================\n");
 	
 //zaczynanie pracy w zależności od wyboru
 switch (stan_pracy){
 
 //opcja KONSOLA
     case KONSOLA:
-
+    //działanie w cmd
         printf ("wybrano pierwsza opcje \n");
-        //  printf("podaj chwile czasowa n: ");
-        // scanf("%d", &czas_dyskretny);
+        printf("podaj chwile czasowa n: ");
+        scanf("%d", &czas_dyskretny);
 
-        // wspl_x=(float)cos((double)czas_dyskretny/4.0);
+        wspl_x=(float)cos((double)czas_dyskretny/4.0);
+        printf("\n=================================\n");
     
-        //printf("wyniki obliczen n dla czasu dyskretnego %d: x: %.2f, y: %.2f  \n", czas_dyskretny, wspl_x,wspl_y);
+        printf("wyniki obliczen n dla czasu dyskretnego %d: x: %.2f, y: %.2f  \n", czas_dyskretny, wspl_x,wspl_y);
+    //porownanie funkcji z funkcja z exela
+        printf("porownanie z funkcja w arkuszu exel... \n");
+        FILE *plik= fopen("arkusz_exel", "r");
+       
+            if(plik == NULL){//jesli plik sie nie otwiera
+
+            printf("\n=================================\n");
+
+                printf("Nie mozna otworzyc pliku.\n");
+
+            printf("\n=================================\n");
+            sleep(3);
+            return 1;
+            }
+        //jesli sie otwiera:    
+        char linia[256];// przechowywanie linii z pliku
+        int obecna_linia=0;
+
+
 
 	break;
 
@@ -59,7 +81,7 @@ switch (stan_pracy){
 //inny wybor
 	default:
 
-    printf("nieprawidlowa opcja!! \n");
+        printf("nieprawidlowa opcja!! \n");
 
 	break;
 
