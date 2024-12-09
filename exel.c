115a1d6be833cb74b8c61fbee870e5d9aa58ac87
stanislaw_janocha <janochastanislaw2004@gmail.com>
2024-12-09 21:14:29 +0100

    pętle for i do... while

--- exel.c
+++ exel.c
@@ -28,27 +28,50 @@ int main(void){
         double wspl_x;
         double wspl_y=0; 
 
-    //definicja zmiennych char
-        char linia[100];
-
     //wypisanie opcji do wyboru
-    
+   do{
             printf("wybierz opcje: \n");
             printf("1. KONSOLA \n");
             printf("2. PLIK \n");
             printf("3. Wyjscie \n");
             printf("\n=================================\n");
             scanf("%d", (int* )&stan_pracy);
-
-    printf("\n=================================\n");
-	
+            printf("\n=================================\n");
+            if (stan_pracy<1||stan_pracy>3) {
+            printf("Niepoprawna wartosc! Wpisz 1,2,3, aby kontynuowac.\n\n");
+   }
+}while (stan_pracy < 1 || stan_pracy > 3);
+    
 //zaczynanie pracy w zależności od wyboru
 switch (stan_pracy){
 
 //opcja KONSOLA
     case KONSOLA:
+   
     //działanie w cmd
         printf ("wybrano pierwsza opcje \n");
+        printf("podaj chwile czasowa n: ");
+        scanf("%d", &czas_dyskretny);
+ for(czas_dyskretny==1;czas_dyskretny<39;czas_dyskretny++){
+        wspl_x=(float)cos((double)czas_dyskretny/4.0);
+        wspl_y=(float)sin((double)czas_dyskretny/4.0);
+        printf("\n=================================\n");
+    
+        printf("wyniki obliczen n dla czasu dyskretnego %d: x: %.2f, y: %.2f  \n", czas_dyskretny, wspl_x,wspl_y);
+
+}
+        wspl_x=(float)cos((double)czas_dyskretny/4.0);
+        wspl_y=(float)sin((double)czas_dyskretny/4.0);
+        printf("\n=================================\n");
+    
+        printf("wyniki obliczen n dla czasu dyskretnego %d: x: %.2f, y: %.2f  \n", czas_dyskretny, wspl_x,wspl_y);
+    
+
+	break;
+
+//opcja PLIK
+	case PLIK:
+
         printf("podaj chwile czasowa n: ");
         scanf("%d", &czas_dyskretny);
 
@@ -57,58 +80,25 @@ switch (stan_pracy){
     
         printf("wyniki obliczen n dla czasu dyskretnego %d: x: %.2f, y: %.2f  \n", czas_dyskretny, wspl_x,wspl_y);
 
-//porownanie funkcji z funkcja z exela
-FILE *plikexel= fopen("arkusz_exel.csv", "r");
-
-
-        if(plikexel == NULL){//jesli plik exel sie nie otwiera
-
+        FILE *plikwyniki = fopen("wyniki.csv", "w"); // Otwórz plik w trybie dopisywania
+        if(plikwyniki==NULL){
             printf("\n=================================\n");
 
             printf("Nie mozna otworzyc pliku.\n");
 
             printf("\n=================================\n");
 
-            sleep(10);
-
-                
-        return 1;
+            sleep(3);
         }
-
-        //jesli sie otwiera: 
             printf("\n=================================\n");
-            
             printf("udalo sie otworzyc plik \n");
             printf("\n=================================\n");
+        for(czas_dyskretny=1; czas_dyskretny<39;czas_dyskretny++){
+        wspl_x=(float)cos((double)czas_dyskretny/4.0);
+
+        fprintf(plikwyniki,"x:%.2f y: %.2f\n\n =======\n",wspl_x,wspl_y);
         
-        while (fgets(linia,sizeof(linia),plikexel)){
-
-            double wartosci[100];
-            int kolumna=0;        
-            
-            char *token = strtok(linia, ",");
-            
-        while (token) {
-            wartosci[kolumna++] = strtof(token, NULL); // Konwersja na double
-            token = strtok(NULL, ",");
-            }
-                    if (wartosci[0] == czas_dyskretny) {
-            printf("Wartosc w ostatniej kolumnie tego wiersza: %.2f\n", wartosci[kolumna - 1]);
-            fclose(plikexel); // Zamknij plik przed zakończeniem programu
-
-            sleep(10);
-            return 0;
-        }
-        }
-        
-
-	break;
-
-//opcja PLIK
-	case PLIK:
-
-        printf ("wybrano druga opcje \n");
-
+        }fclose(plikwyniki);
 	break;
 
 //opcja WYJSCIE
@@ -126,8 +116,9 @@ FILE *plikexel= fopen("arkusz_exel.csv", "r");
 	break;
 
   }
-  sleep(10);
+  sleep(3);
      return 0; 
     }
+
 
 
